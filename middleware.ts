31c173184bc9173ae5002b7fc85189820f282442
delalelouis <delale.louis@gmail.com>
2026-03-25 18:23:12 +0100

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
+import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
 
 export async function middleware(request: NextRequest) {
-  let response = NextResponse.next({
-    request,
-  });
+  let response = NextResponse.next();
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -14,18 +12,12 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(
-          cookiesToSet: { name: string; value: string; options?: CookieOptions }[]
-        ) {
-          cookiesToSet.forEach(({ name, value, options }) => {
+        setAll(cookies) {
+          cookies.forEach(({ name, value }) => {
             request.cookies.set(name, value);
           });
-
-          response = NextResponse.next({
-            request,
-          });
-
-          cookiesToSet.forEach(({ name, value, options }) => {
+          response = NextResponse.next();
+          cookies.forEach(({ name, value, options }) => {
             response.cookies.set(name, value, options);
           });
         },
@@ -33,28 +25,7 @@ export async function middleware(request: NextRequest) {
     }
   );
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  const pathname = request.nextUrl.pathname;
-
-  const isLoginPage = pathname === "/login";
-  const isHtmlPage = pathname.endsWith(".html");
-  const isPublicFile =
-    pathname.startsWith("/_next") || pathname.startsWith("/favicon.ico");
-
-  if (isPublicFile) {
-    return response;
-  }
-
-  if (!session && !isLoginPage && !isHtmlPage) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  if (session && isLoginPage) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  await supabase.auth.getSession();
 
   return response;
 }
